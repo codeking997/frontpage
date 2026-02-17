f34aa9f1277c503a2b1910e21d141e53bdbfdb59
codeking997 <york@live.no>
2026-02-17 12:56:38 +0100

    update color

--- frontpage-1/src/components/Navbar.tsx
+++ frontpage-1/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import reactLogo from '../assets/react.svg'
 
 export default function Navbar() {
     return (
-        <header className="flex items-center bg-blue-500 text-black p-16">
+        <header className="flex items-center bg-[#21222A] text-[#61DAFB] p-16">
             <a href="https://react.dev" rel="noopener noreferrer" target="_blank" aria-label='react website'>
           <img src={reactLogo} className="logo react" alt="React logo" />
          </a>
